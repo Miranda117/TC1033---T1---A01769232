52ecbc89fa6224a809f55173c39a9d09bbfd5e2f
Miran117 <a01769232@itesm.mx>
2019-11-13 21:37:57 -0600

    CORRECCIONES DE PORCENTAJES
    Se corrigieron los porcentajes y asignaron los nombres correspondientes

--- SRC/solucion.py
+++ SRC/solucion.py
@@ -8,8 +8,57 @@ def construir_diccionario():
     for l in lineas:
         datos= l.split(",") 
         vuelo= datos[0]
+        vuelo=str(vuelo[0:2])
         mes = datos[2].replace("{"," ")
         mes=str(mes[3:5])
+        if mes =="01":
+            mes="Enero"
+        if mes =="02":
+            mes="Febrero"
+        if mes =="03":
+            mes="Marzo"
+        if mes =="04":
+            mes="Abril"
+        if mes =="05":
+            mes="Mayo"
+        if mes =="06":
+            mes="Junio"
+        if mes =="07":
+            mes="julio"
+        if mes =="08":
+            mes="Agosto"
+        if mes =="09":
+            mes="Septiembre"
+        if mes =="10":
+            mes="Octubre"
+        if mes =="11":
+            mes="Noviembre"
+        if mes =="12":
+            mes="Diciembre"
+        if vuelo=="A6":
+            vuelo="Emiratos Árabes"
+        if vuelo=="D9":
+            vuelo="Alemania"
+        if vuelo=="PP":
+            vuelo="Brasil"
+        if vuelo=="CF":
+            vuelo="Canada"
+        if vuelo=="A7":
+            vuelo="Catar"
+        if vuelo=="HS":
+            vuelo="Tailandea"
+        if vuelo=="9V":
+            vuelo="Singapur"
+        if vuelo=="XA":
+            vuelo="México"
+        if vuelo=="JA":
+            vuelo="Japon"
+        if vuelo=="EN":
+            vuelo="Español"
+        if vuelo=="PK":
+            vuelo="Indonesia"
+        if vuelo=="EN":
+            vuelo="Español"
         if mes not in mes_total:
             mes_total[mes]=1
             Pais_vuelos[mes]={}
@@ -37,10 +86,10 @@ def procentajes():
         procen[mes]={}
         for vuelo in Pais_vuelos[mes].keys():
             narch=round((((Pais_vuelos[mes][vuelo])/mes_total[mes])*100),2)
-            if narch>=20:
+            if narch >= 20:
                 procen[mes][vuelo]=narch
-            elif narch<20:
-                procen[mes][vuelo]="No alcanza el %20"
+            else:
+                procen[mes][vuelo]= "No alcanzo el %20"
             
             archivo.write(mes)
             archivo.write(",")
