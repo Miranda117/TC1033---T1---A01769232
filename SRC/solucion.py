fcf7440b5b87d48bc19604aecb06f9e7720b7f94
Miran117 <a01769232@itesm.mx>
2019-11-13 19:50:42 -0600

    archivo "resultados"
    se crea un archivo llamado resultados y modificamos porcentajes

--- SRC/solucion.py
+++ SRC/solucion.py
@@ -3,26 +3,22 @@ def construir_diccionario():
     Pais_vuelos = {}
     mes_total={}
     archivo=open("datos_vuelos.csv","r+")
-    lineas  = archivo.readlines()
-    lineas.pop(0) 
+    lineas=archivo.readlines()
+    lineas.pop(0)
     for l in lineas:
         datos= l.split(",") 
         vuelo= datos[0]
-        vuelos = vuelo.split(";")
         mes = datos[2].replace("{"," ")
         mes=str(mes[3:5])
         if mes not in mes_total:
             mes_total[mes]=1
+            Pais_vuelos[mes]={}
         else :
             mes_total[mes]+=1
-
-        if not mes in Pais_vuelos:
-                Pais_vuelos[mes]={}
-        else:    
-            if vuelo not in Pais_vuelos:
-                Pais_vuelos[mes][vuelo]= 1
-            else : 
-                Pais_vuelos[mes][vuelo]+=1
+        if vuelo not in Pais_vuelos[mes].keys():
+            Pais_vuelos[mes][vuelo]=1
+        elif vuelo in Pais_vuelos[mes].keys():
+            Pais_vuelos[mes][vuelo]+=1
     
     print(mes_total)
     print (Pais_vuelos,"\n\n")
@@ -34,27 +30,28 @@ def construir_diccionario():
 def procentajes():
     mes_total,Pais_vuelos=construir_diccionario()
     procen ={}
+    archivo=open("resultados.csv","w+")
+    archivo.write("mes,pais,porcentaje\n")
+    
     for mes in Pais_vuelos.keys():
         procen[mes]={}
         for vuelo in Pais_vuelos[mes].keys():
-            valor=((Pais_vuelos[mes][vuelo]/mes_total[mes])*100)
-            if valor>=20:
-                procen[mes][vuelo]=valor
+            narch=round((((Pais_vuelos[mes][vuelo])/mes_total[mes])*100),2)
+            if narch>=20:
+                procen[mes][vuelo]=narch
+            elif narch<20:
+                procen[mes][vuelo]="No alcanza el %20"
+            
+            archivo.write(mes)
+            archivo.write(",")
+            archivo.write(vuelo)
+            archivo.write(",")
+            archivo.write(str(procen[mes][vuelo]))
+            archivo.write("\n")
+    archivo.close
+    print(procen)
+    return procen
 
 
-    return procen,mes,vuelo 
 
-
-def escribir ():
-    mes,procen=procentajes()
-    
-
-    archivo=open("resultados.csv","w+")
-    archivo.write("Mes, País, porcentaje  ")
-    for mes in procen:
-        archivo.write(mes)
-        archivo.write("\n")
-    
-
-
-escribir()
+procentajes()
